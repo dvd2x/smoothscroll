f11587fd92083c2f7d2043b290e0516df3e33e64
David Piper <black.god1930@gmail.com>
2019-09-17 10:24:08 -0400

    Updates

---
+++ precache-manifest.c65517e5790090180eadcacf73fdce84.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fab0476d7992558a4a622fdda9a4b9",
+    "url": "/smoothscroll/index.html"
+  },
+  {
+    "revision": "21a484e42c0f8d41d086",
+    "url": "/smoothscroll/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "38db717099749ed1181f",
+    "url": "/smoothscroll/static/css/main.c71018a0.chunk.css"
+  },
+  {
+    "revision": "21a484e42c0f8d41d086",
+    "url": "/smoothscroll/static/js/2.3f1b59e4.chunk.js"
+  },
+  {
+    "revision": "38db717099749ed1181f",
+    "url": "/smoothscroll/static/js/main.dfde86f8.chunk.js"
+  },
+  {
+    "revision": "e617f027dd4d9c206a7d",
+    "url": "/smoothscroll/static/js/runtime~main.802c6a90.js"
+  },
+  {
+    "revision": "1fda96b312290778c86ecbc29db5fde2",
+    "url": "/smoothscroll/static/media/David1.1fda96b3.png"
+  },
+  {
+    "revision": "e1299d6085727d822aec64816037d535",
+    "url": "/smoothscroll/static/media/David3.e1299d60.png"
+  },
+  {
+    "revision": "8868b3dee9cfa17a30279af4fc184b35",
+    "url": "/smoothscroll/static/media/burgerbuilder.8868b3de.png"
+  },
+  {
+    "revision": "7ab6bc509e752cee69be7287aab0f00b",
+    "url": "/smoothscroll/static/media/detroitskyline.7ab6bc50.png"
+  },
+  {
+    "revision": "64aa0f2d1ff345406570bb30daefc5d8",
+    "url": "/smoothscroll/static/media/logo.64aa0f2d.png"
+  },
+  {
+    "revision": "27c6513581c947a6b554d5ef73f83761",
+    "url": "/smoothscroll/static/media/parksidebags.27c65135.png"
+  },
+  {
+    "revision": "1d7ebda371e3c2906eeb9987222b7019",
+    "url": "/smoothscroll/static/media/portfolio.1d7ebda3.png"
+  },
+  {
+    "revision": "4938b19be207ebc88f37efcbc5f6f40b",
+    "url": "/smoothscroll/static/media/quizapp.4938b19b.png"
+  },
+  {
+    "revision": "47959398334e799fdab449d19bfc9e2f",
+    "url": "/smoothscroll/static/media/utubegrabber.47959398.png"
+  },
+  {
+    "revision": "3b1787dd02019e866de51e898b973b2b",
+    "url": "/smoothscroll/static/media/wisenote.3b1787dd.png"
+  }
+]);
